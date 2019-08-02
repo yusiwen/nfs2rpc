bc0f8e8256590f6d200307ce83eee9c0674862ff
Siwen Yu <yusiwen@gmail.com>
2019-08-03 01:30:46 +0800

    test(example): Catch exception when call remote proxy

--- example/src/main/java/code/google/nfs/rpc/example/TestMinaRCPClient.java
+++ example/src/main/java/code/google/nfs/rpc/example/TestMinaRCPClient.java
@@ -7,11 +7,17 @@ public class TestMinaRCPClient {
   public static void main(String[] args) {
 
     MinaClientProxy proxy = new MinaClientProxy("192.168.2.54", 18888);
-    HelloWorldService service = (HelloWorldService) proxy.getServiceProxy("helloworld", Codecs.HESSIAN_CODEC, HelloWorldService.class);
+    proxy.addMethodTimeout("*", 20000);
+    HelloWorldService service = (HelloWorldService) proxy.getServiceProxy("helloworld", Codecs.HESSIAN_CODEC,
+        HelloWorldService.class);
 
-    // Use proxy to call
-    String result = service.sayHello("hello");
-    System.out.println(result);
+    try {
+      // Use proxy to call
+      String result = service.sayHello("hello");
+      System.out.println(result);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
     proxy.shutdown(false);
   }
